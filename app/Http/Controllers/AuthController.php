dfb5deea9358c13d4a2b88488998a8bc6b96eb2c
Ays1234 <yogimaulana100@gmail.com>
2023-03-03 10:46:32 +0700

    sudah fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,7 +12,7 @@ class AuthController extends Controller
     //
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login']]);
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
     }
 
     public function register()
@@ -22,10 +22,11 @@ class AuthController extends Controller
         'email' => 'required|email|unique:users',
          'password' => 'required']);
     if($validator->fails()){
-        return return response()->json($validator->messages());
+        return response()->json($validator->messages());
     }
 
-    $user =User::create(['name' => request('name'),
+    $user =User::create([
+        'name' => request('name'),
     'email' => request('email'),
     'password' => Hash::make(request('password')),
 ]);
